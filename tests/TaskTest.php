9362a414f48d6cba31419339d38ff28cdf6f42a9
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-02-19 16:57:31 -0800

    pass first test. add files and server integration.

---
+++ tests/TaskTest.php
@@ -0,0 +1,51 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Task.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class TaskTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_save()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $test_task = new Task($description);
+
+            //Act
+            $test_task->save();
+
+            //Assert
+            $result = Task::getAll();
+            $this->assertEquals($test_task, $result[0]);
+        }
+        function test_getAll()
+        {
+            //Arrange
+            $description = "Wash the dog";
+            $description2 = "Water the lawn";
+            $test_task = new Task($description);
+            $test_task->save();
+            $test_task2 = new Task($description2);
+            $test_task2->save();
+
+            //Act
+            $result = Task::getAll();
+
+            //Assert
+            $this->assertEquals([$test_task, $test_task2], $result);
+        }
+    }
+
+
+?>
