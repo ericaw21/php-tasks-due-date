7fc02725aa5d42646884293b789b67c2d15d7db5
Charles Peden and Erica Wright <charles@>
2017-02-21 08:31:48 -0800

    add duedate variable to task class, getter and setter, and duedate variable to to_do database

--- src/Task.php
+++ src/Task.php
@@ -3,19 +3,17 @@
     {
         private $description;
         private $category_id;
+        private $due_date;
         private $id;
 
-        function __construct($description, $id = null, $category_id)
+        function __construct($description, $id = null, $category_id, $due_date)
         {
             $this->description = $description;
             $this->id = $id;
             $this->category_id = $category_id;
+            $this->due_date = $due_date;
         }
 
-        function setDescription($new_description)
-        {
-            $this->description = (string) $new_description;
-        }
 
         function getDescription()
         {
@@ -32,45 +30,62 @@
             return $this->category_id;
         }
 
+        function getDueDate()
+        {
+            return $this->due_date;
+        }
+
+        static function getAll()
+        {
+          $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+          $tasks = array();
+          foreach($returned_tasks as $task) {
+            $description = $task['description'];
+            $id = $task['id'];
+            $category_id = $task['category_id'];
+            $new_task = new Task($description, $id, $category_id);
+            array_push($tasks, $new_task);
+          }
+          return $tasks;
+        }
+
+        static function find($search_id)
+        {
+          $found_task = null;
+          $tasks = Task::getAll();
+          foreach($tasks as $task) {
+            $task_id = $task->getId();
+            if ($task_id == $search_id) {
+              $found_task = $task;
+            }
+          }
+          return $found_task;
+        }
+
+        function setDescription($new_description)
+        {
+          $this->description = (string) $new_description;
+        }
+
+        function setDueDate($new_due_date)
+        {
+            $this->due_date = $new_due_date;
+        }
+
         function save()
         {
             $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
 
             // NOTE {$this->getCategoryId()} does not have quotes because you are looking to return an integer !!!
-            
+
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
-        static function getAll()
-        {
-            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-            $tasks = array();
-            foreach($returned_tasks as $task) {
-                $description = $task['description'];
-                $id = $task['id'];
-                $category_id = $task['category_id'];
-                $new_task = new Task($description, $id, $category_id);
-                array_push($tasks, $new_task);
-            }
-            return $tasks;
-        }
 
         static function deleteAll()
         {
           $GLOBALS['DB']->exec("DELETE FROM tasks;");
         }
 
-        static function find($search_id)
-        {
-            $found_task = null;
-            $tasks = Task::getAll();
-            foreach($tasks as $task) {
-                $task_id = $task->getId();
-                if ($task_id == $search_id) {
-                  $found_task = $task;
-                }
-            }
-            return $found_task;
-        }
     }
 ?>
